7a6378f255f50688a9cb1bfb379b1d4e76492b9c
MohannadNaj <me@mohannad.io>
2018-09-23 04:21:33 +0300

    WIP

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 let EventBusInstance;
-let windowObject = null;
 
-import Vue from 'vue'
 import EventBus from './EventBus'
 
-EventBusInstance = new EventBus(Vue);
-
-export default EventBusInstance;
+export default EventBus;
